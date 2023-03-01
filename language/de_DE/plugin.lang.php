3dda508ba29bbfb4593acffde5fae77a30d0f72d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-01 15:12:07 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'Wendet automatisch generierte Tags auf Bilder an';
+$lang['Language code'] = 'Sprachcode';
+$lang['Loading... %d1/%d2 processed photos'] = 'Aufladung... %d1/%d2 verarbeitete Fotos';
+$lang['Maximum number'] = 'Maximale Anzahl';
+$lang['Maximum number of tags'] = 'Maximale Anzahl von Tags';
+$lang['Proposed tags'] = 'Vorgeschlagene Tags';
+$lang['Tag Recognition'] = 'Tag-Erkennung';
+$lang['Tag successfully generated and added'] = 'Tag erfolgreich generiert und hinzugefügt';
+$lang['Tags successfully added'] = 'Tags erfolgreich hinzugefügt';
+$lang['There is an error with the API'] = 'Es gibt einen Fehler mit der API
+';
+$lang['Use this API'] = 'Diese API verwenden';
+$lang['With %s API'] = 'Mit der API von %s';
+$lang['You haven\'t enough request left this month for this action'] = 'Sie haben diesen Monat nicht mehr genug Anfragen für diese Aktion';
+$lang['Apply tags'] = 'Tags anwenden';
+$lang['Click on tags to select them'] = 'Tags anklicken, um sie auszuwählen';
+$lang['Fetching the API...'] = 'Abrufen der API...';
+$lang['Generate tags'] = 'Tags generieren';
+$lang['Generate tags for this image'] = 'Tags für dieses Bild generieren';
