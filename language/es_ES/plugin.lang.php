28407b6e59c4a5e00d9994e8e8c1669e9aa42394
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-02 06:43:22 +0100

    Update es_ES, thanks to Tirs

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'Aplica etiquetas auto-generadas a las imágenes';
+$lang['Apply tags'] = 'Aplicar etiquetas';
+$lang['Click on tags to select them'] = 'Pulse sobre las etiquetas para seleccionarlas';
+$lang['Fetching the API...'] = 'Recuperando la API...';
+$lang['Generate tags'] = 'Generar etiquetas';
+$lang['Generate tags for this image'] = 'Generar etiquetas para esta imagen';
+$lang['Language code'] = 'Código de idioma';
+$lang['Loading... %d1/%d2 processed photos'] = 'Carga... %d1/%d2 fotos procesadas';
+$lang['Maximum number'] = 'Número máximo';
+$lang['Maximum number of tags'] = 'Número máximo de etiquetas';
+$lang['Proposed tags'] = 'Etiquetas propuestas';
+$lang['Tag Recognition'] = 'Reconocimiento de etiquetas';
+$lang['Tag successfully generated and added'] = 'Etiqueta generada y añadida correctamente';
+$lang['Tags successfully added'] = 'Etiquetas añadidas correctamente';
+$lang['There is an error with the API'] = 'Hay un error con la API';
+$lang['Use this API'] = 'Utilizar esta API';
+$lang['With %s API'] = 'Con la API de %s';
+$lang['You haven\'t enough request left this month for this action'] = 'No le quedan suficientes solicitudes este mes para esta acción';
