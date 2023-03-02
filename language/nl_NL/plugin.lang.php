1cb469a494be1fd6921e473185abd3cba0c1b14b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-02 06:43:11 +0100

    Update nl_NL, thanks to watertoren5044

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['You haven\'t enough request left this month for this action'] = 'Je hebt deze maand niet genoeg aanvragen over voor deze actie.';
+$lang['With %s API'] = 'Met de API van %s';
+$lang['Use this API'] = 'Gebruik deze API';
+$lang['There is an error with the API'] = 'Er is een fout opgetreden met de API';
+$lang['Tags successfully added'] = 'Tags succesvol toegevoegd';
+$lang['Tag successfully generated and added'] = 'Tag succesvol gegenereerd en toegevoegd';
+$lang['Tag Recognition'] = 'Tagherkenning';
+$lang['Proposed tags'] = 'Voorgestelde tags';
+$lang['Maximum number of tags'] = 'Maximaal aantal tags';
+$lang['Maximum number'] = 'Maximaal aantal';
+$lang['Loading... %d1/%d2 processed photos'] = 'Chargement... %d1/%d2 verwerkte foto\'s';
+$lang['Language code'] = 'Taalcode';
+$lang['Generate tags for this image'] = 'Genereer tags voor deze afbeelding';
+$lang['Generate tags'] = 'Tags genereren';
+$lang['Fetching the API...'] = 'De API ophalen...';
+$lang['Click on tags to select them'] = 'Klik op tags om ze te selecteren';
+$lang['Apply tags'] = 'Tags toepassen';
+$lang['Applies auto-generated tags to images'] = 'Past automatisch gegenereerde tags toe op afbeeldingen';
