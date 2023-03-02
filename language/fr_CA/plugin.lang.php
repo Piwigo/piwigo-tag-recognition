161088832be857e946f86508f185b97b20ab9e67
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-02 17:59:23 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'Appliquer des mots-clés générés aux images';
+$lang['Apply tags'] = 'Appliquer les mots-clés';
+$lang['Click on tags to select them'] = 'Cliquez sur les mots-clés pour les sélectionner';
+$lang['Generate tags'] = 'Générer des mots-clés';
+$lang['Generate tags for this image'] = 'Générer des mots-clés pour cette image';
+$lang['Maximum number'] = 'Nombre maximum de mot-clé';
+$lang['Maximum number of tags'] = 'Nombre maximum de mots-clés';
+$lang['Proposed tags'] = 'Mots-clés Proposés';
+$lang['Tag Recognition'] = 'Génération des mots-clés';
+$lang['Tag successfully generated and added'] = 'Les mots-clés ont été générés et ajoutés aux images';
+$lang['Tags successfully added'] = 'Les mots-clés ont été ajoutés';
+$lang['You haven\'t enough request left this month for this action'] = 'Vous n\\'avez pas assez de requête restante ce mois-ci pour effectuer cette action';
