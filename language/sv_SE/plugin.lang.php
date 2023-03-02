b3fabbe522f3263be30b653fc665c1f92e4f2339
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-02 06:42:59 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'Tillämpar automatiskt genererade taggar på bilder';
+$lang['Apply tags'] = 'Tillämpa taggar';
+$lang['Click on tags to select them'] = 'Klicka på taggar för att välja dem';
+$lang['Fetching the API...'] = 'Hämtar API...';
+$lang['Generate tags'] = 'Generera taggar';
+$lang['Generate tags for this image'] = 'Generera taggar för denna bild';
+$lang['Language code'] = 'Språkkod';
+$lang['Loading... %d1/%d2 processed photos'] = 'Läser in... %d1/%d2 bearbetade foton';
+$lang['Maximum number'] = 'Maximalt antal';
+$lang['Maximum number of tags'] = 'Maximalt antal taggar';
+$lang['Proposed tags'] = 'Föreslagna taggar';
+$lang['Tag Recognition'] = 'Taggigenkänning';
+$lang['Tag successfully generated and added'] = 'Taggen har skapats och lagts till';
+$lang['Tags successfully added'] = 'Taggen har lagts till';
+$lang['There is an error with the API'] = 'Det är ett fel med API';
+$lang['Use this API'] = 'Använd detta API';
+$lang['With %s API'] = 'Med %s\'s API';
+$lang['You haven\'t enough request left this month for this action'] = 'Du har inte tillräckligt med förfrågningar kvar denna månad för den här åtgärden';
