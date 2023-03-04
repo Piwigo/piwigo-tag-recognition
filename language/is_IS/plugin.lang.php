3eae5f0ab65d4139af0d0502c1e6fda35ed9356a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-04 20:07:12 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'Notar sjálfvirkt útbúin merki á myndir';
+$lang['Apply tags'] = 'Notaðu merki';
+$lang['Click on tags to select them'] = 'Smelltu á merki til að velja þau';
+$lang['Fetching the API...'] = 'Sækir API...';
+$lang['Generate tags'] = 'Búðu til merki';
+$lang['Generate tags for this image'] = 'Búðu til merki fyrir þessa mynd';
+$lang['Language code'] = 'Tungumálakóði';
+$lang['Loading... %d1/%d2 processed photos'] = 'Hleðsla... %d1/%d2 unnum myndum';
+$lang['Maximum number'] = 'Hámarksfjöldi';
+$lang['Maximum number of tags'] = 'Hámarksfjöldi merkja';
+$lang['Proposed tags'] = 'Framlögð merki';
+$lang['Tag Recognition'] = 'Merkja auðkenning';
+$lang['Tag successfully generated and added'] = 'Merki búið til og bætt við';
+$lang['Tags successfully added'] = 'Merkjum bætt við';
+$lang['There is an error with the API'] = 'Það er villa með API';
+$lang['Use this API'] = 'Notaðu þetta API';
+$lang['With %s API'] = 'Með API fyrir %s';
+$lang['You haven\'t enough request left this month for this action'] = 'Þú átt ekki nóg eftir af beiðnum í þessum mánuði fyrir þessa aðgerð';
