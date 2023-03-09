863d62ed3a08fef2837dbb00932285b2c9b43198
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-09 06:23:56 +0100

    Update da_DK, thanks to Kaare

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'Tilføjer autogenererede tags til billeder';
+$lang['Apply tags'] = 'Tilføj tags';
+$lang['Click on tags to select them'] = 'Klik på tags for at vælge dem';
+$lang['Fetching the API...'] = 'Henter et API ...';
+$lang['Generate tags'] = 'Generer tags';
+$lang['Generate tags for this image'] = 'Genererer tags til dette billede';
+$lang['Language code'] = 'Sprogkode';
+$lang['Loading... %d1/%d2 processed photos'] = 'Indlæser ... %d1/%d2 behandlede fotografier';
+$lang['Maximum number'] = 'Maksimalt antal';
+$lang['Maximum number of tags'] = 'Maksimalt antal tags';
+$lang['Proposed tags'] = 'Foreslåede tags';
+$lang['Tag Recognition'] = 'Taggenkendelse';
+$lang['Tag successfully generated and added'] = 'Tag genereret og tilføjet';
+$lang['Tags successfully added'] = 'Tag tilføjet';
+$lang['There is an error with the API'] = 'Der opstod en fejl i API\'et';
+$lang['Use this API'] = 'Anvend dette API';
+$lang['With %s API'] = 'With %ss API';
+$lang['You haven\'t enough request left this month for this action'] = 'Du har ikke nok forespørgsler tilbage i denne måned til at udføre handlingen';
