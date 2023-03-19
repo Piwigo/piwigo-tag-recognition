6c707dce8433f78132d996b2c2fb1d2466a9bdb0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-19 06:26:34 +0100

    Update cs_CZ, thanks to MiPi

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'Aplikuje automaticky generované štítky na obrázky';
+$lang['Apply tags'] = 'Použít štítky';
+$lang['Click on tags to select them'] = 'Kliknutím na štítky je vyberete';
+$lang['Fetching the API...'] = 'Načítá se rozhraní API...';
+$lang['Generate tags'] = 'Generovat štítky';
+$lang['Generate tags for this image'] = 'Generovat štítky pro tento obrázek';
+$lang['Language code'] = 'Kód jazyka';
+$lang['Loading... %d1/%d2 processed photos'] = 'Načítá se... %d1/%d2 zpracovaných fotografií';
+$lang['Maximum number'] = 'Maximální počet';
+$lang['Maximum number of tags'] = 'Maximální počet štítků';
+$lang['Proposed tags'] = 'Navrhované štítky';
+$lang['Tag Recognition'] = 'Rozpoznávání štítků';
+$lang['Tag successfully generated and added'] = 'Štítek byl úspěšně vygenerován a přidán';
+$lang['Tags successfully added'] = 'Štítek byl úspěšně přidán';
+$lang['There is an error with the API'] = 'V rozhraní API došlo k chybě';
+$lang['Use this API'] = 'Použijte toto API';
+$lang['With %s API'] = 'S API od %s';
+$lang['You haven\'t enough request left this month for this action'] = 'Na tuto akci vám tento měsíc nezbylo dost žádostí';
