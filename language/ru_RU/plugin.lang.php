9645fc8c78859805a9489a5d3b0101ae905c5297
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-08 17:22:16 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'Применяет автоматически сгенерированные теги к изображениям';
+$lang['Apply tags'] = 'Применить теги';
+$lang['Click on tags to select them'] = 'Нажмите на теги, чтобы выбрать их';
+$lang['Fetching the API...'] = 'Получение API...';
+$lang['Generate tags'] = 'Сгенерировать теги';
+$lang['Generate tags for this image'] = 'Сгенерировать теги для этого изображения';
+$lang['Language code'] = 'Код языка';
+$lang['Loading... %d1/%d2 processed photos'] = 'Загрузка... %d1/%d2 обработано фотографий';
+$lang['Maximum number'] = 'Максимальное количество';
+$lang['Maximum number of tags'] = 'Максимальное количество тегов';
+$lang['Proposed tags'] = 'Предлагаемые теги';
+$lang['Tag Recognition'] = 'Распознавание тегов';
+$lang['Tag successfully generated and added'] = 'Тег успешно создан и добавлен';
+$lang['Tags successfully added'] = 'Теги успешно добавлены';
+$lang['There is an error with the API'] = 'Произошла ошибка с API';
+$lang['Use this API'] = 'Используйте этот API';
+$lang['With %s API'] = 'С API %s';
+$lang['You haven\'t enough request left this month for this action'] = 'У вас недостаточно запросов в этом месяце для этого действия';
