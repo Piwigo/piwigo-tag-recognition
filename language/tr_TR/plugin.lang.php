6dfd6a41adb9c3ba15b237919f254918268ba418
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-04-06 11:15:35 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['You haven\'t enough request left this month for this action'] = 'Bu ay bu işlem için yeterli sayıda isteğiniz kalmadı';
+$lang['There is an error with the API'] = 'API ile ilgili bir hata var';
+$lang['Loading... %d1/%d2 processed photos'] = 'Yükleniyor... İşlenen fotoğraflar %d1/%d2';
+$lang['Fetching the API...'] = 'API alınıyor...';
+$lang['Applies auto-generated tags to images'] = 'Görsellere otomatik olarak oluşturulan etiketleri uygular';
+$lang['With %s API'] = '%s\'nin API ile';
+$lang['Use this API'] = 'Bu API\'yi kullan';
+$lang['Tags successfully added'] = 'Etiket başarıyla eklendi';
+$lang['Tag successfully generated and added'] = 'Etiket başarıyla oluşturuldu ve eklendi';
+$lang['Tag Recognition'] = 'Etiket Tanıma';
+$lang['Proposed tags'] = 'Önerilen etiketler';
+$lang['Maximum number of tags'] = 'En fazla etiket sayısı';
+$lang['Maximum number'] = 'En fazla';
+$lang['Language code'] = 'Dil kodu';
+$lang['Generate tags for this image'] = 'Bu görsel için etiketler oluştur';
+$lang['Generate tags'] = 'Etiketler oluştur';
+$lang['Apply tags'] = 'Etiketlere uygula';
+$lang['Click on tags to select them'] = 'Seçmek için etiketlere tıklayın';
