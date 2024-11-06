68573486c42fa176d534c6f1e404e0a0a26b707f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-06 17:41:15 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = '将自动生成的标签应用于图像';
+$lang['Apply tags'] = '应用标签';
+$lang['Click on tags to select them'] = '点击标签来选择它们';
+$lang['Fetching the API...'] = '正在获取 API...';
+$lang['Generate tags'] = '生成标签';
+$lang['Generate tags for this image'] = '为此图像生成标签';
+$lang['Language code'] = '语言代码';
+$lang['Loading... %d1/%d2 processed photos'] = '正在加载... %d1/%d2已处理的照片';
+$lang['Maximum number'] = '最大数量';
+$lang['Maximum number of tags'] = '最大标签数量';
+$lang['Proposed tags'] = '建议的标签';
+$lang['Tag Recognition'] = '标签识别';
+$lang['Tag successfully generated and added'] = '标签已成功生成并添加';
+$lang['Tags successfully added'] = '标签已成功添加';
+$lang['There is an error with the API'] = 'API存在错误';
+$lang['Use this API'] = '使用此API';
+$lang['With %s API'] = '使用 %s 的API';
+$lang['You haven\'t enough request left this month for this action'] = '您本月的剩余请求数不足以执行此操作';
