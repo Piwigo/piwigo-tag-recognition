cb0764058db826ecab0b9951c5452e924edd1f67
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:09:42 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = 'מחיל תגיות שנוצרו אוטומטית לתמונות';
+$lang['Apply tags'] = 'החל תגיות';
+$lang['Click on tags to select them'] = 'לחץ על תגיות כדי לבחור בהן';
+$lang['Fetching the API...'] = 'יובא ה- API...';
+$lang['Generate tags'] = 'צור תגיות';
+$lang['Generate tags for this image'] = 'צור תגיות עבור תמונה זו';
+$lang['Language code'] = 'קוד שפה';
+$lang['Loading... %d1/%d2 processed photos'] = 'טוען... %d1/%d2 תמונות שעובדו';
+$lang['Maximum number'] = 'מספר מרבי';
+$lang['Maximum number of tags'] = 'מספר מרבי של תגיות';
+$lang['Proposed tags'] = 'תגיות מוצעות';
+$lang['Tag Recognition'] = 'זיהוי תגיות';
+$lang['Tag successfully generated and added'] = 'תג נוצר בהצלחה ונוסף';
+$lang['Tags successfully added'] = 'תגיות נוספו בהצלחה';
+$lang['There is an error with the API'] = 'יש שגיאה עם ה- API';
+$lang['Use this API'] = 'השתמש ב- API זה';
+$lang['With %s API'] = 'עם API של %s';
+$lang['You haven\'t enough request left this month for this action'] = 'אין לך מספיק בקשות שנותרו לך החודש עבור פעולה זו';
