5f691ca90644e7e0f753102b7a3119e834b766e2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-09 06:46:39 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Applies auto-generated tags to images'] = '이미지에 자동 생성된 태그 적용';
+$lang['Apply tags'] = '태그 적용';
+$lang['Click on tags to select them'] = '태그를 클릭하여 선택하세요';
+$lang['Fetching the API...'] = 'API 불러오는 중...';
+$lang['Generate tags'] = '태그 생성';
+$lang['Generate tags for this image'] = '이 이미지에 태그 생성';
+$lang['Language code'] = '언어 코드';
+$lang['Loading... %d1/%d2 processed photos'] = '로딩 중... %d1/%d2 장의 사진 처리됨';
+$lang['Maximum number'] = '최대 수';
+$lang['Maximum number of tags'] = '태그 최대 개수';
+$lang['Proposed tags'] = '제안된 태그';
+$lang['Tag Recognition'] = '태그 인식';
+$lang['Tag successfully generated and added'] = '태그 생성 및 추가 성공';
+$lang['Tags successfully added'] = '태그 추가 성공';
+$lang['There is an error with the API'] = 'API에 오류가 발생했습니다';
+$lang['Use this API'] = '이 API 사용';
+$lang['With %s API'] = '%s의 API로';
+$lang['You haven\'t enough request left this month for this action'] = '이 작업에 대해 이번 달 남은 요청 횟수가 부족합니다';
